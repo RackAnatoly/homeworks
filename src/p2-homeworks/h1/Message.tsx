229c1780e5d3db88a00250017570867b46a76681
Anatoli Rak <Rack.anatoly@gmail.com>
2022-02-21 10:25:51 +0100

    1

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import s from './Message.module.css'
+
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
+    return (
+        <div className={s.main}>
+            <div className={s.first}>
+                <img src={props.avatar} className={s.avatar}/>
+            </div>
+            <div className={s.second}>
+                <div className={s.name}>{props.name} </div>
+                <div className={s.message}>{props.message} </div>
+                <div className={s.time}>{props.time} </div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
